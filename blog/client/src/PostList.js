607726c9a671af5f24ebc54492fafb2c6bdb1083
momenelnakeeb <momenelnakeeb7@gmail.com>
2025-06-27 02:43:07 +0300

    adding the commentList to comments

--- blog/client/src/PostList.js
+++ blog/client/src/PostList.js
@@ -1,13 +1,17 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import CommentCreate from "./CommentCreate";
+import CommentList from "./CommentList";
 
+
+// eslint-disable-next-line import/no-anonymous-default-export
 export default () => {
   // it's on array as it defined
   //  in the posts code for post posts
   const [posts, setPosts] = useState({});
 
   const fetchPosts = async () => {
-const res = await axios.get("http://localhost:4000/posts");
+    const res = await axios.get("http://localhost:4000/posts");
     setPosts(res.data);
   };
   // the empty array is telling to the func to run one time
@@ -16,22 +20,24 @@ const res = await axios.get("http://localhost:4000/posts");
   }, []);
 
   console.log(posts);
-  const renderedPosts = Object.values(posts).map(post=> {
+  const renderedPosts = Object.values(posts).map((post) => {
     return (
       <div
         className="card"
         style={{ width: "30%", marginBottom: "20px" }}
-        key = {post.id}
-        >
-
-            <div className="card-body">
-
-        <h3>{post.title}</h3>
-            </div>
+        key={post.id}
+      >
+        <div className="card-body">
+          <h3>{post.title}</h3>
+          <CommentList postId={post.id}/>
+          <CommentCreate postId={post.id}/>
         </div>
+      </div>
     );
   });
-  return <div className="d-flex flex-row flrx-wrap justify-content-between">
-{renderedPosts}
-  </div>;
+  return (
+    <div className="d-flex flex-row flrx-wrap justify-content-between">
+      {renderedPosts}
+    </div>
+  );
 };
