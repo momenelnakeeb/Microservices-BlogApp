739e7d4d069e0d00f1603c6ed5e9f3df02b9fcc6
momenelnakeeb <momenelnakeeb7@gmail.com>
2025-06-26 19:10:22 +0300

    first commit

---
+++ blog/client/src/PostList.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+export default () => {
+  // it's on array as it defined
+  //  in the posts code for post posts
+  const [posts, setPosts] = useState({});
+
+  const fetchPosts = async () => {
+const res = await axios.get("http://localhost:4000/posts");
+    setPosts(res.data);
+  };
+  // the empty array is telling to the func to run one time
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  console.log(posts);
+  const renderedPosts = Object.values(posts).map(post=> {
+    return (
+      <div
+        className="card"
+        style={{ width: "30%", marginBottom: "20px" }}
+        key = {post.id}
+        >
+
+            <div className="card-body">
+
+        <h3>{post.title}</h3>
+            </div>
+        </div>
+    );
+  });
+  return <div className="d-flex flex-row flrx-wrap justify-content-between">
+{renderedPosts}
+  </div>;
+};
